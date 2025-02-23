1d88706c8104da5139c6ec905f71cb6d6423d02c
Guilherme Alexandre <guilhermegrilo2009@hotmail.com>
2025-02-23 14:32:30 -0300

    Adding subscriptions route

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
-import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod'
+import { validatorCompiler, serializerCompiler, ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -11,6 +12,23 @@ app.register(fastifyCors)
 
 app.get('/hello', () => {
   return 'Hello World!'
+})
+
+app.post('/subscriptions', {
+  schema: {
+    body: z.object({
+      name: z.string(),
+      email: z.string().email(),
+    })
+  },
+}, (request, response) => {
+  const { name, email } = request.body
+  
+  // Criação da inscrição no banco de dados
+
+  return response.status(201).send({
+    name, email,
+  })
 })
 
 app.listen({ port: 3333 }).then(() => {
